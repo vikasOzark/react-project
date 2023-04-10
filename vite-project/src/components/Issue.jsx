c2d9dfd36d8b21475bfee7be8b8c9a010197e2fa
Vikas Kumar <80447331+vikasOzark@users.noreply.github.com>
2023-04-10 23:51:10 +0530

    all changes

--- vite-project/src/components/Issue.jsx
+++ vite-project/src/components/Issue.jsx
@@ -6,6 +6,14 @@ import "../index.css";
 export const IssuePage = () => {
   const [isDrop, setDrop] = useState(false);
   const [Assign, setAssign] = useState(false);
+  const [selected, setSelected] = useState([])
+
+
+  const clickHandle = (e) => {
+    setSelected([...selected, e.target.innerText])
+    console.log( selected)
+    // selected()
+  }
 
   return (
     <React.Fragment>
@@ -15,24 +23,18 @@ export const IssuePage = () => {
             type="text"
             placeholder="Add title . . ."
             className="border-2 border-blue-400 rounded w-full p-1"
+            value={selected}
           />
+          
         </div>
 
         <div className="flex gap-2 mt- ">
           <div className="flex gap-2 mt-2 ">
             <div className="">
               <div className="mt-1">
-                <div
-                  onClick={() => {
-                    setDrop(!isDrop);
-                    setAssign(false);
-                  }}
-                  className=" px-4 py-1 rounded-2xl text-center cursor-pointer text-white bg-slate-600"
-                >
-                  Add tags +
-                </div>
+                { <BTN name="Assign +" css='bg-green-300' clickFunc={() => {setDrop(!isDrop);setAssign(false)}} /> }
               </div>
-              {isDrop && <DropDown />}
+              {isDrop && <DropDown oprionCLick={clickHandle} options={['vikas', 'test']} selected={[...selected]}  />}
             </div>
 
             <div className="">
@@ -47,10 +49,11 @@ export const IssuePage = () => {
                   Assign task +
                 </div>
               </div>
-              {Assign && <DropDown />}
+              {Assign && <DropDown options={['vikas', 'test']} selected={selected}  />}
             </div>
           </div>
         </div>
+
         <div className="border bg-white h-72 rounded-md p-2 mt-3 ">
           <textarea
             placeholder="Write something . . . "
@@ -66,26 +69,46 @@ export const IssuePage = () => {
             </button>
           </div>
         </div>
+
       </div>
     </React.Fragment>
   );
 };
 
-export const DropDown = () => {
+
+
+export const DropDown = (props) => {
   return (
-    <div className="absolute bg-sky-400 mt-2 p-2 w-auto rounded  -lg">
-      <div className="border cursor-pointer rounded bg-white w-[16rem] mb-1 p-1">
-        test{" "}
-      </div>
-      <div className="border cursor-pointer rounded bg-white w-[16rem] mb-1 p-1">
-        test{" "}
-      </div>
-      <div className="border cursor-pointer rounded bg-white w-[16rem] mb-1 p-1">
-        test{" "}
-      </div>
-      <div className="border cursor-pointer rounded bg-white w-[16rem] mb-1 p-1">
-        test{" "}
-      </div>
+    <div className="absolute shadow-lg border bg-white mt-2 p-2 w-auto rounded ">
+      
+      {props.options.map((option, index1) => (
+        <span key={index1}>
+          {props.selected.some(select => select === option) ? (
+            <div onClick={props.oprionCLick} className="hover:bg-indigo-700 bg-indigo-500 text-white cursor-pointer font-semibold rounded w-[16rem] mb-1 p-1">
+              {option}
+            </div> 
+          ) : (
+            <div onClick={props.oprionCLick} className="hover:bg-gray-100 cursor-pointer font-semibold rounded w-[16rem] mb-1 p-1">
+              {option}
+            </div>
+          )}
+        </span>
+      ))}
+      
     </div>
   );
 };
+
+
+
+
+export const BTN = (props) => {
+  return (
+    <div
+    onClick={props.clickFunc}
+      className={` px-4 py-1 rounded-2xl text-center cursor-pointer text-white ${props.css} `}>
+      {props.name}
+    </div>
+  )
+}
+
